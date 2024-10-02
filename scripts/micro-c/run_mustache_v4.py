b6e4250add73d7c49b3d94cd68287440a4014c12
jodiejacobs <jomojaco@ucsc.edu>
2024-10-02 12:30:14 -0700

    updated files
    updated files

--- scripts/micro-c/run_mustache_v4.py
+++ scripts/micro-c/run_mustache_v4.py
@@ -17,36 +17,21 @@ gtf_path = '/scratch1/jodie/wolbachia/Dmelanogaster_wMel_RNAseq/reference_genome
 feature_path = '/scratch1/jodie/wolbachia/Micro-C/reference_genomes/FlyBase_Annotations_all.txt'
 output=list(files.keys())
 file_dict = {'1kb':[], '16kb':[], '128kb':[]}
-feature_list = ['ANNOTATION_SYMBOL', 'FEATURE_TYPE', 'GENE_SNAPSHOT', 'GO_BIOLOGICAL_PROCESS', 'GO_CELLULAR_COMPONENT', 'GO_MOLECULAR_FUNCTION', 'LOCATION_ARM', 'LOCATION_MAX', 'LOCATION_MIN', 'LOCATION_STRAND', 'NAME', 'SPECIES_ABBREVIATION', 'SYMBOL', 'UNIPROT_PROTEIN_FAMILY']
 
-# Open feature_path as a pandas DataFrame with gene_id as the index
+# # Open feature_path as a pandas DataFrame with gene_id as the index
 feature_df = pd.read_csv(feature_path, sep='\t')
-# Create a dictionary with gene_id as the index
-feature_df = feature_df.set_index('FBID_KEY')
-feature_df = feature_df[~feature_df.index.duplicated(keep='first')]
-gene_dict = feature_df.to_dict(orient='index')
+feature_df.drop(columns=['LOCATION_ARM', 'LOCATION_MAX', 'LOCATION_MIN', 'LOCATION_STRAND', 'NAME'], inplace=True)
+# feature_dict = feature_df.set_index('FBID_KEY').T.to_dict()
 
-def search_feature_dict(chromosome, start, end):
-    filtered_genes = []
-    for gene_id, gene_data in gene_dict.items():
-        if gene_data['LOCATION_ARM'] == chromosome and int(gene_data['LOCATION_MIN']) > int(start) and int(gene_data['LOCATION_MAX']) < int(end):
-            filtered_genes.append(gene_id)
-    return filtered_genes
+# def search_feature_dict(gene_id):
+#     if gene_id in feature_dict:
+#         return list(feature_dict[gene_id].values())
+#     else:
+#         return []
 
-def create_gene_matrix(gene_ids):
-    # Create an empty matrix with columns as the keys
-    matrix = pd.DataFrame(columns=feature_list)
-
-    # Iterate over the gene_ids
-    for gene_id in gene_ids:
-        # Get the gene data from the gene_dict
-        gene_data = gene_dict[gene_id]
-        # Add the gene data as a new row in the matrix
-        matrix.loc[gene_id] = list(gene_data.values())
-
-    # Return the matrix
-    return matrix
-
+# gene_data = search_feature_dict('FBgn0267928')
+# print(gene_data)
+##['CR46209', 'SO0002127:lncRNA_gene', '-', '-', '-', '-', '2L', '19215967', '19215451', '1', 'long non-coding RNA:CR46209', 'Dmel', 'lncRNA:CR46209', '-']
 
 #Run mustache at 1kb, 16kb, 128kb resolutions:
 for out in output:
@@ -74,30 +59,32 @@ for out in output:
 
 #Function creates bedfiles with chromosome, start pos, end pos, fdr 
 def diffloop_to_bed(filepath):
-    if os.path.isfile(f'{filepath}.bed') == False:
-        bashCommand = f"tail -n +2 {filepath} | cut -f1,2,6,7 > {filepath}.bed"
-        os.system(bashCommand)
-    else:
-        return
+    bashCommand = f"tail -n +2 {filepath} | cut -f1,2,6,7 > {filepath}.bed"
+    os.system(bashCommand)
 
 bedlist=[]
 #Reformat bedfiles from mustache for use with bedtools
 for range in file_dict.keys():
     for file in file_dict[range]:
         diffloop_to_bed(file)
-    if os.path.isfile(f'map/mustache_v4/{range}/diffloop1.bed') == False:
-        bashCommand1 = f"bedtools intersect -a {file_dict[range][0]}.bed -b {file_dict[range][2]}.bed {file_dict[range][4]}.bed {file_dict[range][6]}.bed > map/mustache_v4/{range}/diffloop1.bed"
-        os.system(bashCommand)
-    if os.path.isfile(f'map/mustache_v4/{range}/diffloop2.bed') == False:
-        bashCommand2 = f"bedtools intersect -a {file_dict[range][1]}.bed -b {file_dict[range][3]}.bed {file_dict[range][5]}.bed {file_dict[range][7]}.bed > map/mustache_v4/{range}/diffloop2.bed"
-        os.system(bashCommand2)
+    bashCommand1 = f"bedtools intersect -a {file_dict[range][0]}.bed -b {file_dict[range][2]}.bed {file_dict[range][4]}.bed {file_dict[range][6]}.bed | uniq > map/mustache_v4/{range}/diffloop1.bed"
+    bashCommand2 = f"bedtools intersect -a {file_dict[range][1]}.bed -b {file_dict[range][3]}.bed {file_dict[range][5]}.bed {file_dict[range][7]}.bed | uniq > map/mustache_v4/{range}/diffloop2.bed"
     bedlist.extend([f"map/mustache_v4/{range}/diffloop1.bed",f"map/mustache_v4/{range}/diffloop2.bed"])
+    os.system(bashCommand1)
+    os.system(bashCommand2)
+
+# Function to parse the attributes field
+def parse_attributes(attributes):
+    attr_dict = {}
+    attributes = attributes.strip(';').split('; ')
+    for attribute in attributes:
+        key, value = attribute.split(' ')
+        attr_dict[key] = value.strip('"')
+    return attr_dict
 
 #Function extracts a list of id's to use in PANGEA
 def extract_unique_gene_ids(gtf_file, output_file):
     # Correctly format the awk command with proper escaping
-    if os.path.isfile(output_file) == True:
-        return
     bash_command = f"""awk '{{match($0, /gene_id "([^"]+)"/, arr); if (arr[1] != "") print arr[1]}}' {gtf_file} | sort | uniq > {output_file}"""
     process = subprocess.run(bash_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # print(bash_command)
@@ -106,53 +93,62 @@ def extract_unique_gene_ids(gtf_file, output_file):
     else:
         print(f"Command failed with error: {process.stderr.decode('utf-8')}")
 
-def extract_annotation(bed_file):
-    try:
-        bed = pd.read_csv(bed_file, sep='\t', header=None)
-        bed.columns = ['chromosome', 'start_pos', 'end_pos', 'fdr']
-    except Exception as e:
-        print(f'Error:{e}')
-        return
-
-    # Initialize an empty pandas DataFrame
-    df = pd.DataFrame(columns=['chromosome', 'start_pos', 'end_pos', 'fdr', 'gene_id'] + feature_list)
+def extract_annotation(gtf_file, bed_file):
+    gtf = pd.read_csv(gtf_file, sep='\t', header=None)
+    bed = pd.read_csv(bed_file, sep='\t', header=None)
+    bed.columns = ['chromosome', 'start_pos', 'end_pos', 'fdr']
+    gtf.columns=['chromosome',  'source',   'feature',  'start_pos',    'end_pos',  'score',    'strand',   'frame',    'attribute']
+                #3R             FlyBase     gene        7145880         7150968     .           -           .           gene_id "FBgn0250732"; gene_name "gfzf"; gene_source "FlyBase"; gene_biotype "protein_coding";
     
-    # Iterate over the bed file
+    # Parse the attributes field and expand it into separate columns
+    attributes_df = gtf['attribute'].apply(parse_attributes).apply(pd.Series)
+
+    # Concatenate the original DataFrame with the new attributes DataFrame
+    gtf = pd.concat([gtf.drop(columns=['attribute']), attributes_df], axis=1)
+    
+    # #print(gtf.columns): 
+    # # Index(['chromosome', 'source', 'feature', 'start_pos', 'end_pos', 'score',
+    # #    'strand', 'frame', 'gene_id', 'gene_name', 'gene_source',
+    # #    'gene_biotype', 'transcript_id', 'transcript_name', 'transcript_source',
+    # #    'transcript_biotype', 'exon_number', 'exon_id', 'tag', 'protein_id'],
+    # #   dtype='object')
+    
+    # Initialize an empty pandas DataFrame
+    df = pd.DataFrame(columns=bed.columns.tolist() + gtf.columns.tolist())
+
     for index, row in bed.iterrows():
         # Access the values in each row
         chromosome = row['chromosome']
         start_pos = row['start_pos']
         end_pos = row['end_pos']
         fdr = row['fdr']
+                
+        gtf_filtered = gtf[(gtf['chromosome'] == chromosome) & (gtf['start_pos'] > start_pos) & (gtf['end_pos'] < end_pos)]
+        
+        for index, entry in gtf_filtered.iterrows():
+            # gene = entry['gene_id']
+            # features = feature_dict[gene]
+            # Create a new row with the additional columns
+            new_row = pd.Series([chromosome, start_pos, end_pos, fdr] + entry.tolist(), index=df.columns)
+            # Append the new row to the DataFrame
+            df.loc[len(df.index)] = new_row
+            # Save the DataFrame to a new file
 
-        # Filter the gene_dict for genes within the specified range
-        gene_ids = search_feature_dict(chromosome, start_pos, end_pos)  
-        # Create a new DataFrame with the gene_ids  
-        gene_matrix = create_gene_matrix(gene_ids) 
-        # Add the chromosome, start_pos, end_pos, and fdr to the DataFrame  
-        gene_matrix['chromosome'] = chromosome  
-        gene_matrix['start_pos'] = start_pos
-        gene_matrix['end_pos'] = end_pos
-        gene_matrix['fdr'] = fdr
-        # Add the gene_id to the DataFrame
-        gene_matrix['gene_id'] = gene_ids
-        # Concatenate the new DataFrame with the existing DataFrame
-        df = pd.concat([df, gene_matrix], axis=0, ignore_index=True)
-        # print(df)
-    # Export the DataFrame to a CSV file
-    df.to_csv(f"{bed_file}.annotation", sep='\t', header=df.columns, index=False)
+    df.to_csv(f"{bed_file}.annotation", sep='\t', header=None, index=False)
     print(f"Annotation has been exported to {bed_file}.annotation")
 
+#Filter gtfs by bedfiles and 
+# Filter gtfs by bedfiles
 
 def process_bed(bed):
     bashCommand = f"bedtools intersect -a {gtf_path} -b {bed} > {bed}.gtf"
     os.system(bashCommand)
-    print(f"Annotation has been exported to {bed}.gtf")
-    extract_annotation(bed)
+    extract_annotation(f"{bed}.gtf", bed)
     extract_unique_gene_ids(f"{bed}.gtf", f"{bed}.gene_ids")
 
-    
-
+# for bed in bedlist:
+#     process_bed(bed)
+print(bedlist)
 # Create a ThreadPoolExecutor with the desired number of threads
 with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
     # Submit each bed file to the executor for processing
@@ -160,13 +156,3 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
     
     # Wait for all the futures to complete
     concurrent.futures.wait(futures)
-
-#Concatenate the annotation files into one file for each diffloop (1 and 2)
-loop1=[bedlist[0],bedlist[2],bedlist[4]]   
-loop2=[bedlist[1],bedlist[3],bedlist[5]]
-
-
-bashCommand = f"cat {' '.join([f'{bed}.annotation' for bed in loop1])} > map/mustache_v4/diffloop1.annotation.tsv"
-os.system(bashCommand)
-bashCommand = f"cat {' '.join([f'{bed}.annotation' for bed in loop2])} > map/mustache_v4/diffloop2.annotation.tsv"
-os.system(bashCommand)
